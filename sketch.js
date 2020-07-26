da4030029d7064b44943def6745e482e3cbf2bbc
ishaanjindal15 <67318299+ishaanjindal15@users.noreply.github.com>
2020-07-26 16:57:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var movingrect , fixedrect
+
+function setup() {
+  createCanvas(800,400);
+  movingrect= createSprite (200,200,80,50);
+ fixedrect= createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(0); 
+  movingrect.x= mouseX 
+  movingrect.y= mouseY
+  if(movingrect.x-fixedrect.x<=movingrect.width/2+fixedrect.width/2&& fixedrect.x-movingrect.x<=movingrect.width/2+fixedrect.width/2&&
+    movingrect.y-fixedrect.y<=movingrect.height/2+fixedrect.height/2&& fixedrect.y-movingrect.y<=movingrect.height/2+fixedrect.height/2){
+    movingrect.shapeColor = "red";
+    fixedrect.shapeColor = "red";
+  }
+  else{
+    movingrect.shapeColor= "orange"
+    fixedrect.shapeColor= "purple"
+  }
+  drawSprites();
+}
